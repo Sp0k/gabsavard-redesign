93e5e16464a4e6880836fc262d2e0c5538e34999
Sp0k <gabrielsavard.prog@gmail.com>
2024-06-26 14:55:34 -0300

    feat(home): add salutations component
    - Carried over the salutations component from my previous Astro project
    - Added the component to the index page
    - Made the index page more responsive by adding flex wraps
    
    Signed-off-by: Sp0k <gabrielsavard.prog@gmail.com>

--- src/components/HeroImage.jsx
+++ src/components/HeroImage.jsx
@@ -2,7 +2,7 @@ const HeroImage = () => {
   const currentJob = " Portfolio";
 
   return (
-    <div class="w-[469px] h-[422px]">
+    <div className="w-[469px] h-[422px]">
       <img src="/hero.png" alt="Gab Savard" class="w-[469px] h-386px" />
       <div className="w-[402px] h-9 p-2 border border-neutral-400 justify-start items-center gap-2.5 inline-flex ml-8">
         <div className="w-4 h-4 bg-[#459DDE] border border-[#459DDE]" />
---
+++ src/components/Salutations.jsx
@@ -0,0 +1,26 @@
+const Salutations = () => {
+  const friends = [
+    "friends",
+    "amis",
+    "amigos",
+    "amici",
+    "freunde",
+    "vrienden",
+    "vänner",
+    "venner",
+    "prieteni",
+  ];
+
+  let randomNum;
+  do {
+    randomNum = Math.floor(Math.random() * 10);
+  } while (randomNum > 8);
+
+  return (
+    <b className="font-Nunito font-semibold text-[#459DDE] text-5xl tracking-wider mb-3">
+      {friends[randomNum]}
+    </b>
+  );
+};
+
+export default Salutations;
