f1ca21a3a8736271af2cdfb35dfb7e45c91f08b5
Sp0k <contact@gabsavard.com>
2024-06-27 18:08:27 -0300

    feat(home): add the posts to the latest section
    Signed-off-by: Sp0k <contact@gabsavard.com>

--- src/components/LatestPost.jsx
+++ src/components/LatestPost.jsx
@@ -1,20 +1,23 @@
-const LatestPost = () => {
-  const onClickHandler = () => console.log("Read more!");
+const LatestPost = ({ post }) => {
+  const title = post.data.title;
+  const image = post.data.image;
+  const description = post.data.description;
+  const path = "/posts/" + post.slug;
 
   return (
     <div className="w-fit h-fit border-2 border-neutral-400 mt-6">
-      <div className="w-[340px] h-[201px] bg-white"></div>
+      <img className="w-[340px] h-[201px]" src={image.url} alt={image.alt} />
       <div className="px-4 py-4">
-        <h1 className="font-Nunito text-[#D9D9D9] text-2xl">
-          Automatic Scroll View
-        </h1>
+        <h1 className="font-Nunito text-[#D9D9D9] text-2xl mb-0">{title}</h1>
         <p className="w-[298px] font-Source-Sans-Pro text-base text-neutral-400 my-4">
-          My quick and easy trick to follow the cursor's position in a TextInput
-          in React Native
+          {description}
         </p>
-        <button class="border border-[#459DDE] py-2 px-4 text-[#459DDE] font-Source-Sans-Pro text-base hover:bg-[#459DDE] hover:text-[#252525] transition-all">
+        <a
+          href={path}
+          class="border border-[#459DDE] py-2 px-4 text-[#459DDE] font-Source-Sans-Pro text-base hover:bg-[#459DDE] hover:text-[#252525] transition-all"
+        >
           Read more {`>`}
-        </button>
+        </a>
       </div>
     </div>
   );
