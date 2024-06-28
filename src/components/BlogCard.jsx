746dd706a468b3947f4ae1c39cc1f839391ebe68
Sp0k <contact@gabsavard.com>
2024-06-28 14:19:34 -0300

    style: add box shadow to blog cards
    Signed-off-by: Sp0k <contact@gabsavard.com>

--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -2,22 +2,26 @@ const BlogCard = ({ title, description, url, image }) => {
   const path = "/posts/" + url;
 
   return (
-    <div className="w-[1000px] border-2 border-neutral-400 flex flex-row py-4 px-4 my-4 mx-auto">
-      <div className="w-[331.5px] h-[160px] overflow-hidden border border-neutral-400 my-auto">
-        <img src={image.url} alt={image.src} />
-      </div>
-      <div className="px-4 max-w-[550px]">
-        <div className="text-3xl text-[#D9D9D9] font-Nunito mb-2">{title}</div>
-        <div className="text-xl text-[#D9D9D9] font-Source-Sans-Pro mb-6">
-          {description}
+    <div className="w-[1000px] border-2 border-neutral-400 flex flex-row py-4 px-4 my-4 mx-auto hover:shadow-[#459DDE] hover:shadow-lg transition-all hover:border-[#459DDE]">
+      <a href={path} className="flex flex-row">
+        <div className="w-[331.5px] h-[160px] overflow-hidden border border-neutral-400 my-auto">
+          <img src={image.url} alt={image.src} />
         </div>
-        <a
-          href={path}
-          className="border border-[#459DDE] py-2 px-4 text-[#459DDE] font-Source-Sans-Pro text-base hover:bg-[#459DDE] hover:text-[#252525] transition-all"
-        >
-          Read more {`>`}
-        </a>
-      </div>
+        <div className="px-4 max-w-[550px]">
+          <div className="text-3xl text-[#D9D9D9] font-Nunito mb-2">
+            {title}
+          </div>
+          <div className="text-xl text-[#D9D9D9] font-Source-Sans-Pro mb-6">
+            {description}
+          </div>
+          <a
+            href={path}
+            className="border border-[#459DDE] py-2 px-4 text-[#459DDE] font-Source-Sans-Pro text-base hover:bg-[#459DDE] hover:text-[#252525] transition-all"
+          >
+            Read more {`>`}
+          </a>
+        </div>
+      </a>
     </div>
   );
 };
--- src/components/LatestPost.jsx
+++ src/components/LatestPost.jsx
@@ -5,20 +5,22 @@ const LatestPost = ({ post }) => {
   const path = "/posts/" + post.slug;
 
   return (
-    <div className="w-fit h-fit border-2 border-neutral-400 mt-6">
-      <img className="w-[340px] h-[201px]" src={image.url} alt={image.alt} />
-      <div className="px-4 py-4">
-        <h1 className="font-Nunito text-[#D9D9D9] text-2xl mb-0">{title}</h1>
-        <p className="w-[298px] font-Source-Sans-Pro text-base text-neutral-400 my-4">
-          {description}
-        </p>
-        <a
-          href={path}
-          class="border border-[#459DDE] py-2 px-4 text-[#459DDE] font-Source-Sans-Pro text-base hover:bg-[#459DDE] hover:text-[#252525] transition-all"
-        >
-          Read more {`>`}
-        </a>
-      </div>
+    <div className="w-fit h-fit border-2 border-neutral-400 mt-6 transition-all hover:border-[#459DDE] hover:shadow-[#459DDE] hover:shadow-lg">
+      <a href="path">
+        <img className="w-[340px] h-[201px]" src={image.url} alt={image.alt} />
+        <div className="px-4 py-4">
+          <h1 className="font-Nunito text-[#D9D9D9] text-2xl mb-0">{title}</h1>
+          <p className="w-[298px] font-Source-Sans-Pro text-base text-neutral-400 my-4">
+            {description}
+          </p>
+          <a
+            href={path}
+            class="border border-[#459DDE] py-2 px-4 text-[#459DDE] font-Source-Sans-Pro text-base hover:bg-[#459DDE] hover:text-[#252525] transition-all"
+          >
+            Read more {`>`}
+          </a>
+        </div>
+      </a>
     </div>
   );
 };
