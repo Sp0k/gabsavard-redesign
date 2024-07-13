5e6888663f23526560447182b8fa9d69db594911
Sp0k <contact@gabsavard.com>
2024-07-13 14:46:36 -0300

    style: change the size of scroll view
    Signed-off-by: Sp0k <contact@gabsavard.com>

--- src/components/ScrollView.jsx
+++ src/components/ScrollView.jsx
@@ -6,11 +6,11 @@ const ScrollView = ({ children }) => {
   const year = new Date().getFullYear();
 
   return (
-    <div className="max-w-screen-2xl h-[90vh] md:h-[95vh] rounded-[40px] border-2 border-neutral-400 mx-auto md:mt-[3vh] overflow-hidden">
+    <div className="max-w-screen-2xl h-[100svh] md:h-[95vh] rounded-[40px] border-2 border-neutral-400 mx-auto md:mt-[3vh] overflow-hidden">
       <Navigation />
       <div className="flex flex-row h-[100%] justify-center gap-8 ml-8 items-start">
         <SideBar />
-        <div className="max-w-[1400px] w-full min-h-[500px] h-[80vh] md:max-h-[83vh] overflow-y-scroll scrollbar scroll-smooth">
+        <div className="max-w-[1400px] w-full min-h-[500px] h-[87svh] md:max-h-[83vh] overflow-y-scroll scrollbar scroll-smooth">
           <div className="mr-6 md:mr-0">{children}</div>
           <p className="mx-auto text-[#999999] w-fit font-Source-Sans-Pro text-sm md:text-base py-6">
             © Copyright {year}. Made by Gab Savard.
