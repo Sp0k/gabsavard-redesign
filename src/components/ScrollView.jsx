b8482576580796bf4092c2ec88ce62871cc94cf2
Sp0k <contact@gabsavard.com>
2025-07-20 22:41:50 -0300

    feat(view): adjusted scroll view to use dynamic view height

--- src/components/ScrollView.jsx
+++ src/components/ScrollView.jsx
@@ -6,12 +6,12 @@ const ScrollView = ({ children }) => {
   const year = new Date().getFullYear();
 
   return (
-    <div className="max-w-screen-2xl h-[100svh] md:h-[95svh] rounded-[40px] border-2 border-neutral-400 mx-auto md:mt-[2svh] overflow-hidden overflow-y-auto">
+    <div className="max-w-screen-2xl h-[100dvh] md:h-[95dvh] rounded-[40px] border-2 border-neutral-400 mx-auto md:mt-[2dvh] overflow-hidden overflow-y-auto">
       <div className="overscroll-contain">
         <Navigation />
         <div className="flex flex-row h-[100%] justify-center gap-8 ml-8 items-start md:pr-1">
           <SideBar />
-          <div className="max-w-[1400px] w-full min-h-[500px] h-[87svh] md:max-h-[83svh] overflow-y-scroll scrollbar scroll-smooth overscroll-none">
+          <div className="max-w-[1400px] w-full min-h-[500px] h-[87dvh] md:max-h-[83dvh] overflow-y-scroll scrollbar scroll-smooth overscroll-none">
             <div className="mr-6 md:mr-0">{children}</div>
             <p className="mx-auto text-[#999999] w-fit font-Source-Sans-Pro text-sm md:text-base py-6">
               © Copyright {year}. Made by Gab Savard.
